8924fbf6477049b50ccca6badb11e4d33673ba94
nihelerroui <niheleeroui124@gmail.com>
2020-12-12 15:02:34 +0100

    atelier03

---
+++ src/app/model/produit.model.ts
@@ -0,0 +1,7 @@
+export class Produit {
+    idProduit : number | undefined   ;
+    nomProduit : string | undefined ;
+    prixProduit : number | undefined ;
+    dateCreation : Date | undefined ;
+
+}
